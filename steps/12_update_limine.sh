10bfb5072a9e5aa5576a19d9b2e7012a1ccc63f1
nerdwave-nick <nick@nerdwave.io>
2025-11-16 15:45:31 +0100

    fix: nonzero exit handling for grep

--- steps/12_update_limine.sh
+++ steps/12_update_limine.sh
@@ -3,8 +3,10 @@ sudo limine-update
 echo_s "limine updated"
 
 echo_i ":: removing default limine entries"
+# catch 1 exit for grep in case of no matches
+c1grep() { grep "$@" || test $? = 1; }
 # grab the boot numbers for the default entries if any
-bootnums=($(sudo efibootmgr | grep -E "^Boot[0-9]{4}\*? Arch Linux Limine" || : \
+bootnums=($(sudo efibootmgr | c1grep -E "^Boot[0-9]{4}\*? Arch Linux Limine" \
     | sed 's/^Boot\([0-9]\{4\}\).*/\1/'))
 
 # go over each boot number and remove it
